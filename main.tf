53ec47c9df31c8ced17169bb832c5e441ad59528
Hendrix Roa <hendrix.alex87@gmail.com>
2020-08-25 19:30:28 -0500

    Support TF version 0.12

--- main.tf
+++ main.tf
@@ -8,22 +8,13 @@ resource "aws_acm_certificate" "main" {
 
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
-  validation_record_fqdns = [for record in aws_route53_record.main : record.fqdn]
+  validation_record_fqdns = [aws_route53_record.main.fqdn]
 }
 
 resource "aws_route53_record" "main" {
-  for_each = {
-    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  name    = each.value.name
-  records = [each.value.record]
-  type    = each.value.type
-
+  name    = aws_acm_certificate.main.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.main.domain_validation_options.0.resource_record_type
   zone_id = var.route53_zone
+  records = [aws_acm_certificate.main.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }
